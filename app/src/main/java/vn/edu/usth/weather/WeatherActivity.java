30a1598dbc116f71f2f6fc1a50bff13c979d4500
DaoHaiLong <daohailong.06122000@gmail.com>
2021-10-27 18:04:04 +0700

    submit prac 15,16,17

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,14 +1,18 @@
 package vn.edu.usth.weather;
 
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -16,7 +20,29 @@ import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class WeatherActivity extends AppCompatActivity {
     public static final String mess = "Android";
@@ -38,6 +64,8 @@ public class WeatherActivity extends AppCompatActivity {
 
         MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.music);
         mp.start();
+        RunImag();
+        getAPIWeatherJson();
 
 
     }
@@ -48,11 +76,13 @@ public class WeatherActivity extends AppCompatActivity {
         return true;
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.refresh:
                 Refresh();
+//                RunImag();
                 break;
 
             case R.id.settings:
@@ -128,9 +158,9 @@ public class WeatherActivity extends AppCompatActivity {
     final Handler ProgressHandlerMessage = new Handler(Looper.getMainLooper()) {
 
         public void handleMessage(Message msg) {
-                String content = msg.getData().getString("Server_Response");
-                Toast toast = Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT);
-                toast.show();
+            String content = msg.getData().getString("Server_Response");
+            Toast toast = Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT);
+            toast.show();
         }
     };
 
@@ -183,16 +213,17 @@ public class WeatherActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(Integer integer) {
 //                String content = "Finished delay Android";
-                Toast toast = Toast.makeText(getApplicationContext(),  "Finished delay Android", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getApplicationContext(), "Finished delay Android", Toast.LENGTH_SHORT);
                 toast.show();
             }
 
             @Override
             protected void onProgressUpdate(Integer... values) {
 //                String content = "Uppdate  Android";
-                Toast toast = Toast.makeText(getApplicationContext(), "Uppdate  Android",+ values[0]);
+                Toast toast = Toast.makeText(getApplicationContext(), "Uppdate  Android", +values[0]);
                 toast.show();
             }
+
             @SuppressLint("StaticFieldLeak")
             @Override
             protected Integer doInBackground(Integer... integers) {
@@ -212,4 +243,105 @@ public class WeatherActivity extends AppCompatActivity {
             }
         };
     }
+
+    public void RunImag() {
+//        prac 15
+//        AsyncTask<String, Integer, Bitmap> tsk = new AsyncTask<String, Integer, Bitmap>() {
+//            Bitmap bitmap1;
+//
+//            @SuppressLint("StaticFieldLeak")
+//            @Override
+//            protected Bitmap doInBackground(String... strings) {
+//                try {
+//                    Thread.sleep(1000);
+//                    URL url = new URL(strings[0]);
+//
+//                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//                    connection.setRequestMethod("GET");
+//                    connection.setDoInput(true);
+//                    connection.connect();
+//
+//                    InputStream inputstream = connection.getInputStream();
+//                    bitmap1 = BitmapFactory.decodeStream(inputstream);
+//
+//                    connection.disconnect();
+//                } catch (MalformedURLException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                return bitmap1;
+//            }
+//
+//            @Override
+//            protected void onPreExecute() {
+//
+//            }
+//
+//            @Override
+//            protected void onProgressUpdate(Integer... values) {
+//                super.onProgressUpdate(values);
+//            }
+//
+//            @Override
+//            protected void onPostExecute(Bitmap bitmap) {
+////                        Toast.makeText(getApplicationContext(), "something beyond the sky", Toast.LENGTH_LONG).show();
+//                ImageView logo = (ImageView) findViewById(R.id.logo);
+//                logo.setImageBitmap(bitmap);
+//            }
+//        };
+//        tsk.execute("https://usth.edu.vn/uploads/logo_moi-eng.png");
+
+//        prac 16
+        RequestQueue Requestqueue = Volley.newRequestQueue(this);
+// a listener (kinda similar to onPostExecute())
+        Response.Listener<Bitmap> listeners = new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        ImageView iv = (ImageView) findViewById(R.id.logo);
+                        iv.setImageBitmap(response);
+                    }
+                };
+// a simple request to the required image
+        ImageRequest imageRequest = new ImageRequest(
+                "https://usth.edu.vn/uploads/logo_moi-eng.png",
+                listeners, 1, 0, ImageView.ScaleType.CENTER,
+                Bitmap.Config.ARGB_8888,null);
+// go!
+        Requestqueue.add(imageRequest);
+    }
+//    Prac 17
+    private void getAPIWeatherJson()
+    {
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=e3fc7cd1499bf87b25c7829f2ff41639";
+        RequestQueue Requestqueue = Volley.newRequestQueue(this);
+        JsonObjectRequest js = new JsonObjectRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        TextView textview = (TextView) findViewById(R.id.city_name);
+                        String out = "";
+                        try {
+                            String name = response.getString("name");
+                            double temp = response.getJSONObject("main").getDouble("temp");
+
+                            out = name + "\n" +  String.valueOf(temp) + "Long";
+
+                        } catch (  JSONException e) {
+                            e.printStackTrace();
+                        }
+                        textview.setText(out);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+               Toast toast= Toast.makeText(getApplicationContext(), "Fail to get data.....", Toast.LENGTH_SHORT);
+               toast.show();
+            }
+        });
+        Requestqueue.add(js);
+   }
 }
